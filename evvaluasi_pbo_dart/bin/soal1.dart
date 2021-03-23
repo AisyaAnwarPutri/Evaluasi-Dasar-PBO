55c872d2b856198a8520813572aceefcdcb9b692
Aisya <aisyanwarputri@gmail.com>
2021-03-23 10:08:01 +0700

    soal1

--- evvaluasi_pbo_dart/bin/soal1.dart
+++ evvaluasi_pbo_dart/bin/soal1.dart
@@ -1,5 +1,22 @@
-import 'package:evvaluasi_pbo_dart/evvaluasi_pbo_dart.dart' as evvaluasi_pbo_dart;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${evvaluasi_pbo_dart.calculate()}!');
+  Manusia Mahasiswa;
+  String nama = stdin.readLineSync();
+  int nim = int.tryParse(stdin.readLineSync());
+  int nilai_angka = int.tryParse(stdin.readLineSync());
+  String grade_nilai = stdin.readLineSync();
+
+  Mahasiswa = Manusia();
+  print("Nama: ");
+  Mahasiswa.nama = stdin.readLineSync();
+
+  // print("Nama: " + Mahasiswa.nama);
+  print("Nim: " + nim.toString());
+  print("Nilai Angka: " + nilai_angka.toString());
+  print("Grade Nilai: " + grade_nilai.toString());
+}
+
+class Manusia {
+  String nama;
 }
