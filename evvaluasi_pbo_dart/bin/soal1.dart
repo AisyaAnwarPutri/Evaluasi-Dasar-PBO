96d0d6e8dc79a106571554d7d3a1dae9f5a9f92e
Aisya <aisyanwarputri@gmail.com>
2021-03-23 09:17:38 +0700

    evaluasi pbo dart

---
+++ evvaluasi_pbo_dart/bin/soal1.dart
@@ -0,0 +1,5 @@
+import 'package:evvaluasi_pbo_dart/evvaluasi_pbo_dart.dart' as evvaluasi_pbo_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${evvaluasi_pbo_dart.calculate()}!');
+}
---
+++ evvaluasi_pbo_dart/test/evvaluasi_pbo_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:evvaluasi_pbo_dart/evvaluasi_pbo_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
